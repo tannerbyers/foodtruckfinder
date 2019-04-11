e754e764e4ab182756b110d12e26ecf1799c6880
Tanner Byers <programtanner@gmail.com>
2019-04-11 17:30:20 -0400

    Added Marker w/ Text

--- src/Components/ListedTrucks.js
+++ src/Components/ListedTrucks.js
@@ -5,34 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import React from "react";
 
-const TestData = [
-  {
-      Name: "Sweeto Burrito",
-      Location: [30.237247, -81.519488],
-      Pic: "google.com",
-      Days: [ 1, 3, 5 ],
-      Hours: "10 AM - 1 PM"
-  },
- {
-      Name: "Chinchillas",
-      Location: [30.237247, -81.519488],
-      Pic: "google.com",
-      Days: [ 1, 3, 5 ],
-      Hours: "10 AM - 1 PM"
-  },
-  {
-      Name: "Mamas Food Truck",
-      Location: [30.237247, -81.519488],
-      Pic: "google.com",
-      Days: [ 1, 3, 5 ],
-      Hours: "10 AM - 1 PM"
-  }
-]
-const ListedTrucks = () => {
+const ListedTrucks = (props) => {
   return (
     <div className="container" style={{ width: '50vh' }}>
       <ListGroup>
-      {TestData.map((result, i) => (
+      {props.Data.map((result, i) => (
         <ListGroupItem >{result.Name}</ListGroupItem>
       ))}
       </ListGroup>
