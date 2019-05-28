ba11f5892cb91fdfb56629a29c1831754df31513
Tanner Byers <programtanner@gmail.com>
2019-05-27 22:50:30 -0400

    Updates

---
+++ precache-manifest.83d472d4bd71cebbc24de39e3eb471a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a2d1d6aca8e34275a51",
+    "url": "/foodtruckfinder/static/js/runtime~main.bdb5bc14.js"
+  },
+  {
+    "revision": "de4049b932ce5fb8be80",
+    "url": "/foodtruckfinder/static/js/main.159fc26d.chunk.js"
+  },
+  {
+    "revision": "1b5d7ba3d51791f4d66e",
+    "url": "/foodtruckfinder/static/js/2.4149e333.chunk.js"
+  },
+  {
+    "revision": "de4049b932ce5fb8be80",
+    "url": "/foodtruckfinder/static/css/main.99a3b038.chunk.css"
+  },
+  {
+    "revision": "1b5d7ba3d51791f4d66e",
+    "url": "/foodtruckfinder/static/css/2.98a2868c.chunk.css"
+  },
+  {
+    "revision": "a1327df68c2696bd315bbfd6eae0abe8",
+    "url": "/foodtruckfinder/index.html"
+  }
+];
