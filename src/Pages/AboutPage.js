b34c73215e3dc6010daf73864c5889e077f364f2
tannerbyers <programtanner@gmail.com>
2019-12-28 23:22:20 -0500

    updated Pages

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Container, Row, Col} from "reactstrap"
+import FoodtruckIcon from "../assets/food-truck.png"
 
 class AboutPage extends React.Component {
  
@@ -14,12 +16,21 @@ class AboutPage extends React.Component {
     return (
 	<div className="App">
 	<h1>About</h1> 
-	<p className="p-5"> Jacksonville food trucks are always on the move, and that’s why we’ve created the most reliable food truck schedule in Jacksonville, FL. Our Jacksonville food truck schedule updates as the food truck owners make changes to their calendars. Food Truck Finder is the only resource that shows you where Jacksonville food trucks are located at any given time!
+	<Container>
+	<Row>
+	<Col>
+	<img src={FoodtruckIcon} />
+	</Col>
+	<Col>
+	<p className="p-5 "> Jacksonville food trucks are always on the move, and that’s why we’ve created the most reliable food truck schedule in Jacksonville, FL. Our Jacksonville food truck schedule updates as the food truck owners make changes to their calendars. Food Truck Finder is the best resource that shows you where Jacksonville food trucks are located at any given time!
 
 Hungry yet? Track down a Jacksonville food truck near you!
 
 Oh, and don’t forget to bookmark this page</p>	
-</div>
+	</Col>
+	</Row>
+	</Container> 
+	</div>
     )
   }
 }
--- src/Pages/ContactMe.js
+++ src/Pages/ContactMe.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
 class ContactPage extends React.Component {
   
 componentDidMount(){
@@ -10,7 +13,46 @@ componentDidMount(){
   }
  
  render() {
-    return <h1>Contact Page :D </h1>
+    return ( 
+	<div>
+	<h1>Contact Me </h1>
+	<h4 className="Email" > You can email me at anytime at <b> <a href="mailto:jaxfoodtruckfinder@gmail.com" target="_top">jaxfoodtruckfinder@gmail.com</a></b> or submit the form below with your inquiry!</h4>
+    <Container className="px-5 py-5">
+    <Form className="justify-content-center">
+      <FormGroup>
+        <Label for="exampleEmail">Contact Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="(Optional)" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleText">Message</Label>
+        <Input type="Message" name="text"  id="exampleText" required/>
+      </FormGroup>
+      <FormGroup tag="fieldset">
+        <legend>Message Type</legend>
+        <FormGroup inline check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Issue
+          </Label>
+        </FormGroup>
+        <FormGroup inline check>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+           Praise 
+          </Label>
+        </FormGroup>
+        <FormGroup check inline>
+          <Label check>
+            <Input type="radio" name="radio1" />{' '}
+            Other
+          </Label>
+        </FormGroup>
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+	</ Container>
+	</div>
+     )
   }
 }
 export default ContactPage
